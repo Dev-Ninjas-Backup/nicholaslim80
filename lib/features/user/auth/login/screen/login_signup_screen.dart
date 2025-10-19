22e31122efe7e77db8ff38c5ee0715a35f81bc88
AvijitDasAvi <avijitavi338895@gmail.com>
2025-10-19 06:43:45 +0600

    Login and signup screen done.

---
+++ lib/features/user/auth/login/screen/login_signup_screen.dart
@@ -0,0 +1,306 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:nicholaslim80/core/utils/constants/app_colors.dart';
+import 'package:nicholaslim80/core/utils/constants/image_path.dart';
+import 'package:nicholaslim80/features/user/auth/login/controller/login_signup_controller.dart';
+
+class LoginSignupScreen extends StatelessWidget {
+  const LoginSignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(LoginSignupController());
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            ImagePath.backgroundImage,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 20,
+                  ),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Obx(() {
+                    final isLogin = controller.isLoginSelected.value;
+
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          isLogin ? 'Welcome Back!' : 'Welcome!',
+                          style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          isLogin
+                              ? 'Log in to continue delivering with ease.'
+                              : 'Sign up to continue delivering with ease.',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.subtitleFontColor,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+
+                        // Login / Signup Toggle
+                        AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOut,
+                          padding: EdgeInsets.fromLTRB(
+                            isLogin ? 6 : 12,
+                            6,
+                            isLogin ? 12 : 6,
+                            6,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            border: Border.all(
+                              color: AppColors.subtitleFontColor,
+                              width: 1,
+                            ),
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              GestureDetector(
+                                onTap: () => controller.toggleSelection(true),
+                                child: AnimatedContainer(
+                                  duration: const Duration(milliseconds: 300),
+                                  curve: Curves.easeInOut,
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 18,
+                                    vertical: 5,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: isLogin
+                                        ? AppColors.primaryButtonColor
+                                        : Colors.transparent,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: const Text(
+                                    'Login',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              GestureDetector(
+                                onTap: () => controller.toggleSelection(false),
+                                child: AnimatedContainer(
+                                  duration: const Duration(milliseconds: 300),
+                                  curve: Curves.easeInOut,
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 18,
+                                    vertical: 5,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    color: !isLogin
+                                        ? AppColors.primaryButtonColor
+                                        : Colors.transparent,
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: const Text(
+                                    'Sign Up',
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        if (!isLogin) ...[
+                          Container(
+                            height: 52,
+                            width: double.infinity,
+                            padding: EdgeInsets.symmetric(horizontal: 12),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: AppColors.subtitleFontColor,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: TextField(
+                                controller: controller.nameController,
+                                decoration: const InputDecoration(
+                                  hintText: 'Your Name',
+                                  border: InputBorder.none,
+                                  isDense: true,
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          SizedBox(height: 20),
+                          Container(
+                            height: 52,
+                            width: double.infinity,
+                            padding: const EdgeInsets.symmetric(horizontal: 12),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: AppColors.subtitleFontColor,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Center(
+                              child: TextField(
+                                controller: controller.emailController,
+                                keyboardType: TextInputType.emailAddress,
+                                decoration: const InputDecoration(
+                                  hintText: 'Your e-mail address',
+                                  border: InputBorder.none,
+                                  isDense: true,
+                                ),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                        Obx(
+                          () => controller.isLoginSelected.value
+                              ? Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: const Text(
+                                    'Login with your phone number',
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                )
+                              : const SizedBox.shrink(),
+                        ),
+                        Obx(
+                          () => controller.isLoginSelected.value
+                              ? SizedBox(height: 10)
+                              : SizedBox.shrink(),
+                        ),
+                        Container(
+                          height: 52,
+                          width: double.infinity,
+                          padding: const EdgeInsets.symmetric(horizontal: 12),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: AppColors.subtitleFontColor,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Row(
+                            children: [
+                              DropdownButtonHideUnderline(
+                                child: DropdownButton<String>(
+                                  value: controller.selectedCountry.value,
+                                  items: controller.countries
+                                      .map(
+                                        (country) => DropdownMenuItem(
+                                          value: country,
+                                          child: Text(
+                                            country,
+                                            style: const TextStyle(
+                                              fontSize: 16,
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                      .toList(),
+                                  onChanged: (value) {
+                                    if (value != null) {
+                                      controller.selectCountry(value);
+                                    }
+                                  },
+                                  icon: const Icon(Icons.arrow_drop_down),
+                                ),
+                              ),
+                              const SizedBox(width: 12),
+                              Expanded(
+                                child: TextField(
+                                  controller: controller.phoneController,
+                                  keyboardType: TextInputType.phone,
+                                  decoration: InputDecoration(
+                                    hintText: 'Enter phone number',
+                                    border: InputBorder.none,
+                                    isDense: true,
+                                    suffixIcon:
+                                        controller.phoneNumber.value.isNotEmpty
+                                        ? GestureDetector(
+                                            onTap: controller.clearPhone,
+                                            child: const Icon(
+                                              Icons.clear_outlined,
+                                              size: 20,
+                                            ),
+                                          )
+                                        : null,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 30),
+
+                        // Button
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.primaryButtonColor,
+                            minimumSize: const Size(double.infinity, 50),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            isLogin ? 'Login' : 'Continue',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
