c0d4344a3f22f5b24bb1484ca9334836a02573c5
Saklain <saklain.flutter@gmail.com>
2025-11-02 23:41:20 +0600

    added notification

--- lib/features/user/auth/login/controller/login_signup_controller.dart
+++ lib/features/user/auth/login/controller/login_signup_controller.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nicholaslim80/features/user/bottom_navbar/screen/bottom_navbar_screen.dart';
