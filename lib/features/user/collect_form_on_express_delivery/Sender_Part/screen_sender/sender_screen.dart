4e295f64fea03ca9782bd5ea4efa765e5e8d5055
Kamrulbiswas009 <kamrulbiswas81@gmail.com>
2025-11-14 00:30:45 +0600

    sender & recipient fix responcive

--- lib/features/user/collect_form_on_express_delivery/Sender_Part/screen_sender/sender_screen.dart
+++ lib/features/user/collect_form_on_express_delivery/Sender_Part/screen_sender/sender_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nicholaslim80/core/common/styles/global_text_style.dart';
-
 import 'package:nicholaslim80/core/utils/constants/app_colors.dart';
 import 'package:nicholaslim80/core/utils/constants/image_path.dart';
 import 'package:nicholaslim80/features/user/collect_form_on_express_delivery/Sender_Part/controller_sender/sender_controller.dart';
@@ -9,7 +8,7 @@ import 'package:nicholaslim80/features/user/collect_form_on_express_delivery/Sen
 import 'package:nicholaslim80/features/user/collect_form_on_express_delivery/models/address_model.dart';
 
 class SenderView extends StatelessWidget {
-  final AddressModel address; // store the address
+  final AddressModel address;
 
   const SenderView({super.key, required this.address});
 
@@ -28,131 +27,127 @@ class SenderView extends StatelessWidget {
         ),
       ),
       extendBodyBehindAppBar: true,
-      body: Column(
-        children: [
-          /// MAP SECTION
-          Expanded(
-            child: Image.asset(
-              ImagePath.map,
-              width: double.infinity,
-              fit: BoxFit.cover,
-            ),
-          ),
-
-          /// FORM SECTION
-          Container(
-            padding: EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30),
-                topRight: Radius.circular(30),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              /// MAP SECTION
+              Image.asset(
+                ImagePath.map,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
-            ),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Sender",
-                    style: getTextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    controller: controller.addressController,
-                    label: "Address*",
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    controller: controller.floorController,
-                    label: "Floor or unit no.*",
-                    maxLines: 1,
-                    maxLength: 120,
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    controller: controller.nameController,
-                    label: "Contact name*",
-                    suffixIcon: const Icon(Icons.person_outline),
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    controller: controller.numberController,
-                    label: "Contact number*",
-                    keyboardType: TextInputType.phone,
-                    // maxLengthEnforcement removed
-                  ),
-                  SizedBox(height: 10),
-                  CustomTextField(
-                    controller: controller.noteController,
-                    label: "Note to driver",
-                    maxLines: 2,
-                    maxLength: 120,
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Obx(
-                        () => Checkbox(
-                          value: controller.saveAddress.value,
-                          onChanged: (value) =>
-                              controller.saveAddress.value = value ?? false,
-                        ),
-                      ),
-                      Text(
-                        "Save this address",
-                        style: getTextStyle(
-                          color: Colors.grey.shade600,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 16),
 
-                  /// CONFIRM BUTTON
-                  Obx(
-                    () => SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: controller.isFormValid.value
-                            ? () {
-                                Get.snackbar(
-                                  "Success",
-                                  "Form submitted!",
-                                  snackPosition: SnackPosition.BOTTOM,
-                                );
-                              }
-                            : null,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: controller.isFormValid.value
-                              ? Colors.yellow.shade700
-                              : Colors.grey.shade300,
-                          padding: EdgeInsets.symmetric(vertical: 16),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+              /// FORM SECTION
+              Container(
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Sender",
+                      style: getTextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: controller.addressController,
+                      label: "Address*",
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: controller.floorController,
+                      label: "Floor or unit no.*",
+                      maxLines: 1,
+                      maxLength: 120,
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: controller.nameController,
+                      label: "Contact name*",
+                      suffixIcon: Icon(Icons.person_outline),
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: controller.numberController,
+                      label: "Contact number*",
+                      keyboardType: TextInputType.phone,
+                    ),
+                    SizedBox(height: 10),
+                    CustomTextField(
+                      controller: controller.noteController,
+                      label: "Note to driver",
+                      maxLines: 2,
+                      maxLength: 120,
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      children: [
+                        Obx(
+                          () => Checkbox(
+                            value: controller.saveAddress.value,
+                            onChanged: (value) =>
+                                controller.saveAddress.value = value ?? false,
                           ),
                         ),
-                        child: Text(
-                          "Confirm",
+                        Text(
+                          "Save this address",
                           style: getTextStyle(
-                            fontSize: 18,
-                            color: controller.isFormValid.value
-                                ? Colors.black
-                                : Colors.grey.shade600,
+                            color: Colors.grey.shade600,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16),
+
+                    /// CONFIRM BUTTON
+                    Obx(
+                      () => SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: controller.isFormValid.value
+                              ? () {
+                                  Get.snackbar(
+                                    "Success",
+                                    "Form submitted!",
+                                    snackPosition: SnackPosition.BOTTOM,
+                                  );
+                                }
+                              : null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: controller.isFormValid.value
+                                ? Colors.yellow.shade700
+                                : Colors.grey.shade300,
+                            padding: EdgeInsets.symmetric(vertical: 16),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                          ),
+                          child: Text(
+                            "Confirm",
+                            style: getTextStyle(
+                              fontSize: 18,
+                              color: controller.isFormValid.value
+                                  ? Colors.black
+                                  : Colors.grey.shade600,
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
