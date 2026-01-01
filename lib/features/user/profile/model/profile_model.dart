29babd73fd21d88e8bf4d85fe6637b97c7705f81
SaburNayem <hossennayem099@gmail.com>
2026-01-02 04:44:35 +0600

    got it

--- lib/features/user/profile/model/profile_model.dart
+++ lib/features/user/profile/model/profile_model.dart
@@ -3,4 +3,13 @@ class ProfileModel {
   final String subtitle;
 
   ProfileModel({required this.title, required this.subtitle});
+
+  factory ProfileModel.fromJson(Map<String, dynamic> json) {
+    return ProfileModel(
+      title: json['title'] ?? '',
+      subtitle: json['subtitle'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() => {"title": title, "subtitle": subtitle};
 }
