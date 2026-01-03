2b586167f0b325eda0c5d79856b3a1a98a7b1206
SaburNayem <hossennayem099@gmail.com>
2026-01-04 05:27:56 +0600

    screen issueee

--- lib/core/api_end_point/api_end_point.dart
+++ lib/core/api_end_point/api_end_point.dart
@@ -1,10 +1,11 @@
 class ApiEndPoint {
   static const String baseUrl = 'https://api.zipbee.sg/api/v1';
-  
+
   static const String login = '$baseUrl/auth/login';
   static const String signUp = '$baseUrl/auth/signup';
   static const String forgetPass = '$baseUrl/auth/forgot-password';
   static const String notificationID = '$baseUrl/notifications/{id}';
   static const String notification = '$baseUrl/notifications';
+  static const String logOut = '$baseUrl/auth/logout';
   static const String resetPass = '$baseUrl/auth/forgot/reset-password';
 }
