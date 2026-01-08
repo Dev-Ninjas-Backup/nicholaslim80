d4d3c0e1298693bd6eb33a4ca2b3daf678ac88b0
SaburNayem <hossennayem099@gmail.com>
2026-01-08 06:02:21 +0600

    change login issue

--- lib/features/user/order/model/order_model.dart
+++ lib/features/user/order/model/order_model.dart
@@ -3,42 +3,42 @@ class OrderModel {
   final String status;
   final String date;
   final String pickupAddress;
+  final String senderName;
   final String dropOffAddress;
+  final String deliveryLocation;
   final String vehicleType;
   final double total;
+  final bool showReceipt;
 
   OrderModel({
     required this.orderId,
     required this.status,
     required this.date,
     required this.pickupAddress,
+    required this.senderName,
     required this.dropOffAddress,
+    required this.deliveryLocation,
     required this.vehicleType,
     required this.total,
+    required this.showReceipt,
   });
 
   factory OrderModel.fromJson(Map<String, dynamic> json) {
     return OrderModel(
-      /// 🔹 API has `id`
       orderId: json['id']?.toString() ?? '',
-
-      /// 🔹 API status comes from tab (ONGOING / COMPLETED / CANCELLED)
       status: json['status'] ?? '',
-
-      /// 🔹 API has no date → fallback text
-      date: "ASAP Delivery",
-
-      /// 🔹 API has no pickup address → fallback
-      pickupAddress: "Collect from sender",
-
-      /// 🔹 API has no dropoff address → fallback using stops
-      dropOffAddress: "Deliver to ${json['total_stops'] ?? 1} destination(s)",
-
-      /// 🔹 vehicle_type_id mapping
+      date: json['collect_time'] == "ASAP"
+          ? "Pick-up ASAP"
+          : (json['collect_time'] ?? "Scheduled Delivery"),
+      pickupAddress: json['pickup_address'] ?? "Collect from",
+      senderName: json['sender_name'] ?? "Unknown",
+      dropOffAddress:
+          json['drop_off_address'] ??
+          "Deliver to ${json['total_stops'] ?? 1} destination(s)",
+      deliveryLocation: json['delivery_location'] ?? "",
       vehicleType: json['vehicle_type_id'] == 1 ? "Motorbike" : "Vehicle",
-
-      /// 🔹 API total_cost is STRING
       total: double.tryParse(json['total_cost']?.toString() ?? '0') ?? 0,
+      showReceipt: json['status'] == "COMPLETED",
     );
   }
 }
