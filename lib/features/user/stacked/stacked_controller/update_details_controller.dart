ad0aa2238754ad61b7aa4965b1e31b8c82264331
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2026-01-14 05:37:29 +0600

    updated vehicle

--- lib/features/user/stacked/stacked_controller/update_details_controller.dart
+++ lib/features/user/stacked/stacked_controller/update_details_controller.dart
@@ -173,8 +173,60 @@ class UpdateDetailsController extends GetxController {
     final status = res['statusCode'] as int? ?? 500;
     if (status == 200) {
       _applyServerTotal(res['body'] as Map<String, dynamic>);
+
+      // debug total_cost and total_fee if present
+      try {
+        final b = res['body'] as Map<String, dynamic>;
+        double cost = 0.0;
+        double fee = 0.0;
+        if (b['data'] != null) {
+          final d = b['data'];
+          if (d['total_cost'] != null) cost = double.tryParse(d['total_cost'].toString()) ?? 0.0;
+          else if (d['order'] != null && d['order']['total_cost'] != null) cost = double.tryParse(d['order']['total_cost'].toString()) ?? 0.0;
+          if (d['total_fee'] != null) fee = double.tryParse(d['total_fee'].toString()) ?? 0.0;
+          else if (d['order'] != null && d['order']['total_fee'] != null) fee = double.tryParse(d['order']['total_fee'].toString()) ?? 0.0;
+        }
+        if (b['total_cost'] != null) cost = double.tryParse(b['total_cost'].toString()) ?? cost;
+        if (b['total_fee'] != null) fee = double.tryParse(b['total_fee'].toString()) ?? fee;
+        debugPrint('patchCollectTime parsed total_cost: $cost total_fee: $fee');
+      } catch (_) {}
+
       return true;
     }
+
+    return false;
+  }
+
+  /// Update vehicle type for an existing order
+  Future<bool> patchVehicleType(int orderId, int vehicleTypeId) async {
+    isLoading.value = true;
+    final res = await OrderService.updateOrderDetails(orderId, {'vehicle_type_id': vehicleTypeId});
+    isLoading.value = false;
+
+    debugPrint('patchVehicleType response: ${res['body']}');
+    try {
+      final bodyMap = res['body'] as Map<String, dynamic>? ?? {};
+      if (bodyMap.containsKey('message')) debugPrint('patchVehicleType message: ${bodyMap['message']}');
+    } catch (_) {}
+
+    final status = res['statusCode'] as int? ?? 500;
+    if (status == 200) {
+      _applyServerTotal(res['body'] as Map<String, dynamic>);
+
+      // debug and return total_cost
+      try {
+        final b = res['body'] as Map<String, dynamic>;
+        double cost = 0.0;
+        if (b['data'] != null && b['data']['total_cost'] != null) cost = double.tryParse(b['data']['total_cost'].toString()) ?? 0.0;
+        else if (b['data'] != null && b['data']['order'] != null && b['data']['order']['total_cost'] != null) cost = double.tryParse(b['data']['order']['total_cost'].toString()) ?? 0.0;
+        else if (b['total_cost'] != null) cost = double.tryParse(b['total_cost'].toString()) ?? 0.0;
+
+        debugPrint('patchVehicleType total_cost: $cost');
+      } catch (_) {}
+
+      return true;
+    }
+
     return false;
   }
 }
