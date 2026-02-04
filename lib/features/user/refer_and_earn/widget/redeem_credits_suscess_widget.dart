39a3b6736033d4f35831539b8584efe2d6232816
kamrulbiswas009 <kamrulbiswas81@gmail.com>
2026-02-05 03:08:31 +0600

    Redeem Success Screen auto navigate to BottomNavbarScreen.

--- lib/features/user/refer_and_earn/widget/redeem_credits_suscess_widget.dart
+++ lib/features/user/refer_and_earn/widget/redeem_credits_suscess_widget.dart
@@ -1,11 +1,27 @@
+import 'dart:async';
 import 'package:ZipBee/core/common/styles/global_text_style.dart';
 import 'package:ZipBee/core/utils/constants/app_colors.dart';
+import 'package:ZipBee/features/user/bottom_navbar/screen/bottom_navbar_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
-class RedeemSuccessScreen extends StatelessWidget {
+class RedeemSuccessScreen extends StatefulWidget {
   const RedeemSuccessScreen({super.key});
+
+  @override
+  State<RedeemSuccessScreen> createState() => _RedeemSuccessScreenState();
+}
+
+class _RedeemSuccessScreenState extends State<RedeemSuccessScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 2), () {
+  Get.to(BottomNavbarScreen()); 
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +31,11 @@ class RedeemSuccessScreen extends StatelessWidget {
         backgroundColor: AppColors.backgroungColor,
         elevation: 0,
         leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Get.back(),
         ),
-        
         centerTitle: true,
       ),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -29,14 +43,13 @@ class RedeemSuccessScreen extends StatelessWidget {
             Container(
               width: 80,
               height: 80,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                 color: Colors.lightGreen,
                 shape: BoxShape.circle,
               ),
-              child: Icon(Icons.check, color: Colors.white, size: 40),
+              child: const Icon(Icons.check, color: Colors.white, size: 40),
             ),
-            SizedBox(height: 20),
-
+            const SizedBox(height: 20),
             Text(
               "Credits successfully redeemed to\n your wallet",
               textAlign: TextAlign.center,
