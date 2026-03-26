c09f2da8ccc163ce3aea8743f387f7791bb4af55
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2026-03-27 00:07:14 +0600

    comment unused files

--- lib/v/models/v_models.dart
+++ lib/v/models/v_models.dart
@@ -1,30 +1,30 @@
-import 'package:get/get.dart';
+// import 'package:get/get.dart';
 
-class VModels {
-  final String id;
-  final String name;
-  final String description;
-  final String dimensions;
-  final String imagePath;
-  final double price;
+// class VModels {
+//   final String id;
+//   final String name;
+//   final String description;
+//   final String dimensions;
+//   final String imagePath;
+//   final double price;
 
-  VModels({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.dimensions,
-    required this.imagePath,
-    required this.price,
-  });
-}
+//   VModels({
+//     required this.id,
+//     required this.name,
+//     required this.description,
+//     required this.dimensions,
+//     required this.imagePath,
+//     required this.price,
+//   });
+// }
 
-class ServiceV1 {
-  final String name;
-  final double price;
-  final RxBool isSelected = false.obs;
+// class ServiceV1 {
+//   final String name;
+//   final double price;
+//   final RxBool isSelected = false.obs;
 
-  ServiceV1({
-    required this.name,
-    required this.price,
-  });
-}
+//   ServiceV1({
+//     required this.name,
+//     required this.price,
+//   });
+// }
